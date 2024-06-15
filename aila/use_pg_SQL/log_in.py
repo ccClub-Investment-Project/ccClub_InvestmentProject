9840c2420fb9c36c003612d4df956aef684771b4
aila8913 <aila8913@gmail.com>
2024-06-15 16:57:31 +0800

    created venv

--- aila/use_pg_SQL/log_in.py
+++ aila/use_pg_SQL/log_in.py
@@ -1,20 +1,35 @@
 import os
 import sys
 
-# 获取上一级目录路径
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # noqa: E402
-# 将上一级目录添加到 sys.path
-sys.path.insert(0, parent_dir)  # noqa: E402
 
-# 打印调试信息，确保路径正确
-print(f"Parent directory: {parent_dir}")  # noqa: E402
-print(f"Files in parent directory: {os.listdir(parent_dir)}")  # noqa: E402
+# # 获取上一级目录路径
+# parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # noqa: E402
+# # 将上一级目录添加到 sys.path
+# sys.path.insert(0, parent_dir)  # noqa: E402
+
+# # 打印调试信息，确保路径正确
+# print(f"Parent directory: {parent_dir}")  # noqa: E402
+# print(f"Files in parent directory: {os.listdir(parent_dir)}")  # noqa: E402
 
 
 from sqlalchemy import create_engine
 import os
 from dotenv import load_dotenv
-os.chdir('C:/python-training/ccClub_InvestmentProject/aila/use_pg_SQL')
+# os.chdir('C:/python-training/ccClub_InvestmentProject/aila/use_pg_SQL')
+# C:\python-training\ccClub_InvestmentProject\aila\use_pg_SQL
+
+# 6/15
+# 加載 .env 文件中的環境變量
+load_dotenv()
+
+# 獲取環境變量
+project_dir = os.getenv('PROJECT_DIR')
+
+# 切換到項目目錄
+if project_dir:
+    os.chdir(project_dir)
+else:
+    raise FileNotFoundError("PROJECT_DIR environment variable not set")
 
 
 def log_in_pgSQL():
