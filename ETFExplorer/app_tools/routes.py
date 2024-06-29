6ea445a56b4a756235c88d6e12b907c967c63134
Rekam <wood54sola@gmail.com>
2024-06-29 11:18:25 +0800

    test cache

--- ETFExplorer/app_tools/routes.py
+++ ETFExplorer/app_tools/routes.py
@@ -3,20 +3,33 @@
 
 # 定義路由和視圖函數
 from flask import render_template, redirect, url_for
-from datetime import datetime
-import logging
-from app_tools.plot_creation import create_plot
-from use_api.data_loader import etf_domestic_list, news, graphJSON, refresh_data # 导入预先加载的数据
 
-def init_routes(app):
+from datetime import datetime
+import logging, time
+from app_tools.plot_creation import create_plot
+# 預先把資料讀進來
+from use_api.data_loader import etf_domestic_list, graphJSON, refresh_data # 导入预先加载的数据
+from use_api.data_loader import graphJSON, refresh_data # 导入预先加载的数据
+
+from use_api.data import get_news_data, api_table_data
+
+def init_routes(app, cache):
+
     @app.route("/keep_alive")
     def test():
         return "website Running!!!"
+    
+    @cache.memoize(timeout=86400)
+    def get_news():
+        return get_news_data('台股,美股', True, 25)
+
 
     @app.route('/')
     def index():
-        # 放置etf info列表
-        etf_domestic_count = len(etf_domestic_list)  # 计算 JSON 数据项的数量
+        # 讀取資料
+        news = get_news()
+        # 計算ETF數量
+        etf_domestic_count = len(etf_domestic_list)
         # 定义变量
         items = ['Apple', 'Banana', 'Cherry']
         extra_info = 'This is some extra information.'
@@ -33,6 +46,7 @@ def init_routes(app):
     def refresh():
         logging.info("Refreshing data")
         refresh_data()
+        # cache.clear()  # 清除所有緩存
         return redirect(url_for('index'))
 
 
